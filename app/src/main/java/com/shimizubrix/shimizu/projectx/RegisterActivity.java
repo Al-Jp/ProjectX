5cae4ba7ba1da4a68c19901f0c86410c307230fe
Shimizu <shimizushgk@gmail.com>
2016-04-18 23:00:30 +0900

    Update RegisterActivity, RegisterRequest, dependencies and add LoginActivity.java

--- app/src/main/java/com/shimizubrix/shimizu/projectx/RegisterActivity.java
+++ app/src/main/java/com/shimizubrix/shimizu/projectx/RegisterActivity.java
@@ -1,7 +1,19 @@
 package com.shimizubrix.shimizu.projectx;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +21,64 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText edtFirstName = (EditText) findViewById(R.id.edtFirstName);
+        final EditText edtLastName = (EditText) findViewById(R.id.edtLastName);
+        final EditText edtEmail = (EditText) findViewById(R.id.edtEmail);
+        final EditText edtUsername = (EditText) findViewById(R.id.edtUsername);
+        final EditText edtPassword = (EditText) findViewById(R.id.edtPassword);
+        final EditText edtPasswordConfirm = (EditText) findViewById(R.id.edtConfirmPassword);
+
+        final String password = edtPassword.getText().toString().trim();
+        final String passwordConfirm = edtPasswordConfirm.getText().toString().trim();
+
+        Button btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (allFieldsComplete(edtFirstName, edtLastName, edtEmail, edtUsername, edtPassword) || passwordsIdentical(password, passwordConfirm)) {
+                    String firstName = edtFirstName.getText().toString();
+                    String lastName = edtLastName.getText().toString();
+                    String email = edtEmail.getText().toString();
+                    String username = edtUsername.getText().toString();
+                    String password = edtPassword.getText().toString();
+
+                    Response.Listener<String> listener = new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean success = jsonResponse.getBoolean("success");
+
+                                if (success) {
+                                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                                } else {
+                                    Snackbar.make(findViewById(R.id.coordinatorLayoutRegister), "Registration failed!", Snackbar.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+
+                    RegisterRequest registerRequest = new RegisterRequest(firstName, lastName, email, username, password, listener);
+                    RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                    queue.add(registerRequest);
+                } else {
+                    Snackbar.make(findViewById(R.id.coordinatorLayoutRegister), "Incomplete fields/passwords do not match!", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private boolean allFieldsComplete(EditText edtFirstName, EditText edtLastName, EditText edtEmail,
+                                      EditText edtUsername, EditText edtPassword) {
+      return !(edtFirstName.getText().toString().trim().isEmpty() || edtLastName.getText().toString().trim().isEmpty() || edtEmail.getText().toString().trim().isEmpty() ||
+                edtUsername.getText().toString().trim().isEmpty() || edtPassword.getText().toString().trim().isEmpty());
+
+    }
+
+    private boolean passwordsIdentical(String password, String confirmPassword) {
+        return password.equals(confirmPassword);
     }
 }
--- app/src/main/java/com/shimizubrix/shimizu/projectx/RegisterRequest.java
+++ app/src/main/java/com/shimizubrix/shimizu/projectx/RegisterRequest.java
@@ -4,6 +4,7 @@ import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Created by shimizu on 4/18/16.
@@ -14,6 +15,16 @@ public class RegisterRequest extends StringRequest {
 
     public RegisterRequest(String firstName, String lastName, String email, String username, String password, Response.Listener<String> listener) {
         super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("firstname", firstName);
+        params.put("lastname", lastName);
+        params.put("email", email);
+        params.put("username", username);
+        params.put("password", password);
     }
 
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
 }
